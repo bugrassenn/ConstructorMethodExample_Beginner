e7c7f8d4910d293dac874398926363ef04164850
Bugra Sen <bugra.sen@bil.omu.edu.tr>
2023-07-20 08:50:07 +0300

    Add project files.

---
+++ Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorMethodExample
+{
+    internal class Customer
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public string Job;
+        public int Age;
+
+
+        public Customer(int id, string firstName, string lastName, string job, int age)
+        {
+            Console.WriteLine("Constructor method working"); // sadece bu kısım ekrana basılır
+
+            // sınıftakilerle eşleştirme yaparak main kısımda  id firstname... çağırımı yapabiliriz.
+            // eğer eşitleme yapmazsak bize değer dönmezler. ekrana basılmaz.
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Job = job;
+            Age = age;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorMethodExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer(1, "Alpaslan", "Şen", "Engineer", 25);
+
+
+            // constructorda sınıftan eşitleme yaptığımız için customer.Id bize yukarıda girdiğimiz 1 i döner
+            // FirstName ise bize Alpaslanı döner. eğer eşitleme yapmasaydık bize boş değer dönücekti.
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.FirstName);
+
+            Console.Read();
+        }
+    }
+}
